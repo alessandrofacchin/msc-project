36e6ffba9b73f9687768222df167705368c1094c
Alessandro Facchin <alessandro.facchin@outlook.com>
2021-05-20 09:02:32 +0200

    Edited Lorenz generator to return a numpy

--- test/lorenz/lorenz_generator_test.py
+++ test/lorenz/lorenz_generator_test.py
@@ -4,7 +4,8 @@ import numpy as np
 
 def test_lorenz_generator():
     g = LorenzGenerator()
-    df = g.generate_data()
+    t, Z = g.generate_latent()
 
-    np.testing.assert_almost_equal(df.index[-1], 99.99)
-    np.testing.assert_equal(list(df.columns), ['x', 'y', 'z'])
+    np.testing.assert_equal(t.shape, (np.ceil(1/0.006),))
+    np.testing.assert_equal(Z.shape, (np.ceil(1/0.006), 3))
+    np.testing.assert_almost_equal(t[-1], 0.996)
