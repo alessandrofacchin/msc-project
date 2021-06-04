3335e7a2b5e4fa12ddbfd3dd76f67ddb0ff01eb1
alessandrofacchin <alessandro.facchin@outlook.com>
2021-06-04 16:06:28 +0000

    Simplified utility

--- latentneural/utils/empty_folder.py
+++ latentneural/utils/empty_folder.py
@@ -7,11 +7,7 @@ from .logging import logger
 def empty_folder(folder: str):
     for filename in os.listdir(folder):
         file_path = os.path.join(folder, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            logger.error('Failed to delete %s. Reason: %s' % (file_path, e))
-            raise e
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
