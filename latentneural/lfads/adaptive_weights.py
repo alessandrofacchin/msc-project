0d7a361f7b8195bb87bc392abe8a746a6c1c2ffd
alessandrofacchin <alessandro.facchin@outlook.com>
2021-06-17 15:25:00 +0000

    Introduced third variable in adaptive weights for L2

--- latentneural/lfads/adaptive_weights.py
+++ latentneural/lfads/adaptive_weights.py
@@ -7,19 +7,19 @@ import sys
 class AdaptiveWeights(tf.keras.callbacks.Callback):
   def __init__(
     self,
-    initial: List[tf.Variable]=[1., 1.],
-    min_weight: List[float]=[0., 0.], 
-    max_weight: List[float]=[1., 1.], 
-    update_steps: List[int]=[1, 1], 
-    update_rates: List[float]=[0., 0.], 
-    update_starts: List[int]=[0, 0]):
+    initial: List[tf.Variable]=[1., 1., 1.],
+    min_weight: List[float]=[0., 0., 0.], 
+    max_weight: List[float]=[1., 1., 1.], 
+    update_steps: List[int]=[1, 1, 1], 
+    update_rates: List[float]=[0., 0., 0.], 
+    update_starts: List[int]=[0, 0, 0]):
     """
     Starting from update_starts, it updates based on the step:
 
     weight = (initial_weight + update_rate * (max(0, step - update_start) / update_step)).clip(min_weight, max_weight)
 
     """
-    self.w = tf.Variable(initial, shape=tf.TensorShape(2), trainable=False)
+    self.w = tf.Variable(initial, shape=tf.TensorShape(len(initial)), dtype=tf.float32, trainable=False)
     self.w_initial = np.asarray(initial)
     self.w_min = np.asarray(min_weight)
     self.w_max = np.asarray(max_weight)
@@ -39,7 +39,6 @@ class AdaptiveWeights(tf.keras.callbacks.Callback):
   
   def on_train_batch_begin(self, batch, logs=None):
     self.update_adaptive_weights()
-    tf.print('Weight1', self.w[0], 'Weight2', self.w[1], output_stream=sys.stdout)
     self.batches += 1
 
   def on_train_begin(self, logs=None):
