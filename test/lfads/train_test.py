0c076f056174992e105ac1f752dd3aa14ca05438
alessandrofacchin <alessandro.facchin@outlook.com>
2021-06-17 15:38:46 +0000

    Added tests on training wrapper

--- test/lfads/train_test.py
+++ test/lfads/train_test.py
@@ -3,61 +3,78 @@ import numpy as np
 import pytest
 
 from latentneural import LFADS
-from latentneural.lfads.train import train_step, train_model
+from latentneural.lfads.adaptive_weights import AdaptiveWeights
+from latentneural.lfads.train import train
 
-   
-@pytest.mark.unit
-def test_train_step():
-    neural_data = np.random.binomial(1, 0.5, (10, 100, 50)).astype(float) # trials X time X neurons
-    
-    model = LFADS(neural_space=50)
-    model.build(input_shape=[None] + list(neural_data.shape[1:]))
-
-    optimizer = tf.keras.optimizers.Adam(1e-4)
-
-    train_step(model, neural_data, optimizer)
+@pytest.fixture(scope='function')
+def enable_eager():
+    tf.compat.v1.enable_eager_execution() # this way, line 317 runs
+    return None
 
 @pytest.mark.unit
-def test_train_model_quick():
+def test_train_model_quick(enable_eager):
     neural_data_train = np.random.binomial(1, 0.5, (10, 100, 50)).astype(float) # test_trials X time X neurons
     neural_data_val = np.random.binomial(1, 0.5, (2, 100, 50)).astype(float) # val_trials X time X neurons
     
-    model = LFADS(neural_space=50)
-    model.build(input_shape=[None, 100, 50])
-
-    optimizer = tf.keras.optimizers.Adam(1e-3)
-
-    train_model(
-        model,
-        optimizer,
-        epochs=1,
-        train_dataset=[neural_data_train],
-        val_dataset=[neural_data_val],
-        coefficients=[5,1,1,1]
+    adaptive_weights = AdaptiveWeights(
+        initial=[0.5, 1, 1],
+        min_weight=[0., 0., 0.],
+        max_weight=[1., 1., 1.],
+        update_steps=[1, 2, 1],
+        update_starts=[2, 1, 1],
+        update_rates=[-0.05, -0.1, -0.01]
     )
+
+    model = LFADS(neural_space=50, max_grad_norm=200)
+
+    model.build(input_shape=[None] + list(neural_data_train.shape[1:]))
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(1e-3),
+        loss_weights=adaptive_weights.w
+    )
+
+    model.fit(x=neural_data_train, y=None, callbacks=[adaptive_weights], shuffle=True, epochs=4, validation_data=(neural_data_val, None))
 
 @pytest.mark.regression
 @pytest.mark.slow
-def test_training_regression():
+def test_training_regression(enable_eager):
     neural_data_train = np.random.binomial(1, 0.5, (10, 100, 50)).astype(float) # test_trials X time X neurons
     neural_data_val = np.random.binomial(1, 0.5, (2, 100, 50)).astype(float) # val_trials X time X neurons
     
-    model = LFADS(neural_space=50, behaviour_space=2)
-    model.build(input_shape=[None, 100, 50])
-
-    optimizer = tf.keras.optimizers.Adam(1e-3)
-
-    train_model(
-        model,
-        optimizer,
-        epochs=1000,
-        train_dataset=[neural_data_train],
-        val_dataset=[neural_data_val],
-        coefficients=[5,1,1,1]
+    adaptive_weights = AdaptiveWeights(
+        initial=[1, 0, 0],
+        update_rates=[0, 0.002, 0],
     )
 
-    log_f, _ = model.call(neural_data_train, training=False)
+    model = LFADS(neural_space=50, max_grad_norm=200)
+
+    model.build(input_shape=[None] + list(neural_data_train.shape[1:]))
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(1e-3),
+        loss_weights=adaptive_weights.w
+    )
+
+    model.fit(x=neural_data_train, y=None, callbacks=[adaptive_weights], shuffle=True, epochs=500, validation_data=(neural_data_val, None))
+
+    log_f, _, _ = model.call(neural_data_train, training=False)
 
     probs = 1/(1 + np.exp(-log_f.numpy()))
     
     assert np.corrcoef(probs.flatten(), neural_data_train.flatten())[0,1] > 0 # Rates are correlated with actual spikes
+
+@pytest.mark.unit
+def test_train_wrap(enable_eager):
+    train(
+        model_settings={}, 
+        optimizer=tf.keras.optimizers.Adam(1e-3), 
+        epochs=2, 
+        train_dataset=np.random.binomial(1, 0.5, (100, 100, 50)).astype(float), 
+        val_dataset=np.random.binomial(1, 0.5, (20, 100, 50)).astype(float), 
+        adaptive_weights=AdaptiveWeights(
+            initial=[1, 0, 0],
+            update_rates=[0, 0.002, 0],
+        ),
+        batch_size=20
+    )
