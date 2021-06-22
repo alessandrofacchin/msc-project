d192eeedbf3a9a631db6e913a186ae94fdecf634
alessandrofacchin <alessandro.facchin@outlook.com>
2021-06-22 19:46:08 +0000

    Added adaptive lr, tensorboard, keyboard interrupt

--- latentneural/lfads/train.py
+++ latentneural/lfads/train.py
@@ -4,6 +4,7 @@ import time
 import tensorflow as tf
 from typing import List, Tuple, Optional, Dict, Any
 import numpy as np
+import os
 
 from .lfads import LFADS
 
@@ -12,17 +13,26 @@ tf.config.run_functions_eagerly(True)
 
 def train(model_settings: Dict[str, Any], optimizer: tf.optimizers.Optimizer, epochs: int, 
     train_dataset: tf.Tensor, adaptive_weights: AdaptiveWeights, 
-    val_dataset: Optional[tf.Tensor]=None, batch_size: Optional[int]=None):
+    val_dataset: Optional[tf.Tensor]=None, batch_size: Optional[int]=None, logdir: Optional[str]=None, 
+    adaptive_lr: Optional[dict]=None):
     
     assert len(train_dataset) > 0, ValueError('Please provide a non-empty train dataset')
     dims = train_dataset.shape[1:] 
+    kwargs=dict()
     if val_dataset is not None:
         assert dims == val_dataset.shape[1:], ValueError('Validation and training datasets must have coherent sizes')
+        kwargs = dict(validation_data=(val_dataset, None))
 
     model = LFADS(
         neural_space=dims[-1],
         **model_settings
     )
+
+    callbacks = [adaptive_weights]
+    if logdir is not None:
+        callbacks.append(tf.keras.callbacks.TensorBoard(log_dir=logdir))
+    if adaptive_lr is not None:
+        callbacks.append(tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', **adaptive_lr))
 
     model.build(input_shape=[None] + list(dims))
 
@@ -31,14 +41,17 @@ def train(model_settings: Dict[str, Any], optimizer: tf.optimizers.Optimizer, ep
         loss_weights=adaptive_weights.w
     )
 
-    model.fit(
-        x=train_dataset, 
-        y=None, 
-        callbacks=[adaptive_weights], 
-        shuffle=True, 
-        epochs=epochs, 
-        validation_data=(val_dataset, None),
-        batch_size=batch_size
-    )
+    try:
+        model.fit(
+            x=train_dataset,
+            y=None,
+            callbacks=callbacks,
+            shuffle=True,
+            epochs=epochs,
+            batch_size=batch_size,
+            **kwargs
+        )
+    except KeyboardInterrupt:
+        return model
 
     return model
